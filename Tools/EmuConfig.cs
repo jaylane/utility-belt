c6ce123692d64a735a06de1ab05e3c2d62b8950d
trevis <trevis@haxit.org>
2019-02-14 15:18:08 -0700

    comments

--- Tools/EmuConfig.cs
+++ Tools/EmuConfig.cs
@@ -6,18 +6,23 @@ using VirindiViewService.Controls;
 
 namespace UtilityBelt.Tools {
     class EmuConfig : IDisposable {
+
+        // this will hold a reference to our ui button for UseDeception
         HudButton UIEmuConfigUseDeception { get; set; }
 
         private bool disposed = false;
 
         public EmuConfig() {
+            // Find the button in the view, or create if it doesnt exist (which is bad and shouldnt happen)
             UIEmuConfigUseDeception = Globals.View.view != null ? (HudButton)Globals.View.view["EmuConfigUseDeception"] : new HudButton();
+            // listen to Hit (click) event on the button
             UIEmuConfigUseDeception.Hit += (s, e) => { ToggleConfig("UseDeception"); };
         }
 
-        private void ToggleConfig(string v) {
+        // sends a /config command to toggle a setting
+        private void ToggleConfig(string setting) {
             try {
-                Util.DispatchChatToBoxWithPluginIntercept(string.Format("/config {0}", v));
+                Util.DispatchChatToBoxWithPluginIntercept(string.Format("/config {0}", setting));
             }
             catch (Exception ex) { Util.LogException(ex); }
         }
