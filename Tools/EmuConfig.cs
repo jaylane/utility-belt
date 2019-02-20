5e5d8b07a818974e6b5be96c842c3d2d673c85ea
Schneebly <Caleb@THOR>
2019-02-19 21:46:23 -0500

    added emu-config

--- Tools/EmuConfig.cs
+++ Tools/EmuConfig.cs
@@ -1,22 +1,133 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Threading;
+using System.Timers;
 using System.Linq;
-using System.Text;
+using System.Xml;
+using Decal.Adapter;
+using Decal.Adapter.Wrappers;
+using MyClasses.MetaViewWrappers;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
 using VirindiViewService.Controls;
+using System.Data;
+using System.Data.Linq;
+using UtilityBelt.Constants;
 
 namespace UtilityBelt.Tools {
     class EmuConfig : IDisposable {
 
-        // this will hold a reference to our ui button for UseDeception
-        HudButton UIEmuConfigUseDeception { get; set; }
+        HudList UIEmuConfigList { get; set; }
 
+        DataTable emuConfigDataTable = new DataTable();
+        
         private bool disposed = false;
+        private static readonly Regex EmuConfigRegex = new Regex(@"(?<ConfigHeader>.*) settings:\n(?<ConfigValues>.*)");
+        private static readonly Regex EmuConfigChangedRegex = new Regex(@"Character option (?<changedConfig>.*) is now (?<changedStatus>.*).");
+
 
         public EmuConfig() {
-            // Find the button in the view, or create if it doesnt exist (which is bad and shouldnt happen)
-            UIEmuConfigUseDeception = Globals.View.view != null ? (HudButton)Globals.View.view["EmuConfigUseDeception"] : new HudButton();
-            // listen to Hit (click) event on the button
-            UIEmuConfigUseDeception.Hit += (s, e) => { ToggleConfig("UseDeception"); };
+            UIEmuConfigList = Globals.View.view != null ? (HudList)Globals.View.view["EmuConfigList"] : new HudList();
+            UIEmuConfigList.Click += new HudList.delClickedControl(EmuConfigList_Click);
+            //UIConfigCheckBox = Globals.View.view != null ? (HudCheckBox)Globals.View.view["ConfigCheckBox"] : new HudCheckBox();
+            Globals.Core.ChatBoxMessage += new EventHandler<ChatTextInterceptEventArgs>(Current_ChatBoxMessage);
+            Globals.Core.Actions.InvokeChatParser("/config list");
+
+        }
+
+        private void EmuConfigList_Click(object sender, int row, int col) {
+            try {
+                //Util.WriteToChat("row: " + row + " column:" + col);
+                HudList.HudListRowAccessor myRow = UIEmuConfigList[row];
+                string configName = ((HudStaticText)myRow[0]).Text.Replace(" ", "");
+                //Util.WriteToChat("Toggling " + configName);
+                ToggleConfig(configName);
+            } catch (Exception ex) { Util.LogException(ex); }
+        } 
+
+
+
+        public void Current_ChatBoxMessage(object sender, ChatTextInterceptEventArgs e) {
+            try {
+                Match match = EmuConfigRegex.Match(e.Text);
+                Match configChange = EmuConfigChangedRegex.Match(e.Text);
+
+                if (match.Success) {
+                    //e.Eat = true;
+
+                    string configHeader = match.Groups["ConfigHeader"].Value;
+                    string configValues = match.Groups["ConfigValues"].Value;
+
+                    string[] configValuesArray =  (configValues.Split(','));
+
+                    HudList.HudListRowAccessor newHeaderRow = UIEmuConfigList.AddRow();
+                    ((HudStaticText)newHeaderRow[0]).Text = configHeader.Trim() + " settings";
+                    ((HudStaticText)newHeaderRow[0]).TextColor = System.Drawing.Color.Goldenrod;
+                    ((HudCheckBox)newHeaderRow[1]).Visible = false;
+
+
+                    Util.WriteToChat(configHeader.Trim());
+                    foreach(string configValue in configValuesArray) {
+
+                        if (Enum.IsDefined(typeof(CharOptions),configValue.Trim())) {
+                            int value = (int)Enum.Parse(typeof(CharOptions), configValue.Trim());
+                            //if ((Globals.Core.CharacterFilter.CharacterOptions & 0x00000002) != 0) { "this setting is on" };
+                            if ((Globals.Core.CharacterFilter.CharacterOptions & value) != 0) {
+                                HudList.HudListRowAccessor newValueRow = UIEmuConfigList.AddRow();
+                                ((HudStaticText)newValueRow[0]).Text = "        " + Regex.Replace(configValue, "([A-Z]+)", " $1").Trim();
+                                ((HudCheckBox)newValueRow[1]).Checked = true;
+                                Util.WriteToChat("       " + configValue.Trim());
+                                Util.WriteToChat(configValue.Trim() + " status: On");
+                            } else if (Enum.IsDefined(typeof(CharOptions2), configValue.Trim())) {
+                                HudList.HudListRowAccessor newValueRow = UIEmuConfigList.AddRow();
+                                ((HudStaticText)newValueRow[0]).Text = "        " + Regex.Replace(configValue, "([A-Z]+)", " $1").Trim();
+                                ((HudCheckBox)newValueRow[1]).Checked = true;
+                                Util.WriteToChat("       " + configValue.Trim());
+                                Util.WriteToChat(configValue.Trim() + " status: On");
+                            }
+                            else {
+                                HudList.HudListRowAccessor newValueRow = UIEmuConfigList.AddRow();
+                                ((HudStaticText)newValueRow[0]).Text = "        " + Regex.Replace(configValue, "([A-Z]+)", " $1").Trim();
+                                ((HudCheckBox)newValueRow[1]).Checked = false;
+                                Util.WriteToChat(configValue.Trim() + " status: Off");
+                            }
+                            
+                        }
+                    }
+                }
+                else {
+                    //Util.WriteToChat(e.Text);
+                }
+            if (configChange.Success) {
+                string changedConfig = configChange.Groups["changedConfig"].Value;
+                string changedStatus = configChange.Groups["changedStatus"].Value;
+                bool changedStatusBool;
+                    Util.WriteToChat("changedConfig: " + changedConfig + " changedStatus: " + changedStatus);
+                    if (changedStatus == "on") {
+                        changedStatusBool = true;
+                    } else {
+                        changedStatusBool = false;
+                    }
+
+                    for (int i = 0; i < UIEmuConfigList.RowCount; i++) {
+                        HudList.HudListRowAccessor row = UIEmuConfigList[i];
+                        string configName2 = ((HudStaticText)row[0]).Text.Replace(" ", "");
+                        //Util.WriteToChat("configName : " + configName2);
+                        if (configName2 == changedConfig) {
+                        //    Util.WriteToChat(changedConfig);
+                            ((HudCheckBox)row[1]).Checked = changedStatusBool;
+                        }
+                    }
+                }
+            } catch (Exception ex) { Util.LogException(ex); }
+        }
+
+        //HudList.HudListRowAccessor myRow = UIMyKillTaskList[row];
+        //string configName = ToCamelCase(((HudStaticText)myRow[0]).Text)
+
+        private void ConfigToggled(string setting) {
+            
         }
 
         // sends a /config command to toggle a setting
@@ -30,11 +141,14 @@ namespace UtilityBelt.Tools {
         public void Dispose() {
             Dispose(true);
             GC.SuppressFinalize(this);
+            
         }
 
         protected virtual void Dispose(bool disposing) {
             if (!disposed) {
                 if (disposing) {
+                    Globals.Core.ChatBoxMessage -= new EventHandler<ChatTextInterceptEventArgs>(Current_ChatBoxMessage);
+                    UIEmuConfigList.Click -= new HudList.delClickedControl(EmuConfigList_Click);
                 }
                 disposed = true;
             }
