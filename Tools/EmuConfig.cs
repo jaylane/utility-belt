5756b7c1daf6760b9a4e10e1a2700906f1f9933a
Schneebly <Caleb@THOR>
2019-02-20 17:02:38 -0500

    yes/no working properly

--- Tools/EmuConfig.cs
+++ Tools/EmuConfig.cs
@@ -21,21 +21,26 @@ namespace UtilityBelt.Tools {
         HudList UIEmuConfigList { get; set; }
 
         DataTable emuConfigDataTable = new DataTable();
+        DateTime lastRequestedConfigList;
         
+
+
         private bool disposed = false;
         private static readonly Regex EmuConfigRegex = new Regex(@"(?<ConfigHeader>.*) settings:\n(?<ConfigValues>.*)");
         private static readonly Regex EmuConfigChangedRegex = new Regex(@"Character option (?<changedConfig>.*) is now (?<changedStatus>.*).");
-
+        
 
         public EmuConfig() {
             UIEmuConfigList = Globals.View.view != null ? (HudList)Globals.View.view["EmuConfigList"] : new HudList();
             UIEmuConfigList.Click += new HudList.delClickedControl(EmuConfigList_Click);
             //UIConfigCheckBox = Globals.View.view != null ? (HudCheckBox)Globals.View.view["ConfigCheckBox"] : new HudCheckBox();
             Globals.Core.ChatBoxMessage += new EventHandler<ChatTextInterceptEventArgs>(Current_ChatBoxMessage);
+            lastRequestedConfigList = DateTime.UtcNow;
             Globals.Core.Actions.InvokeChatParser("/config list");
 
         }
 
+        
         private void EmuConfigList_Click(object sender, int row, int col) {
             try {
                 //Util.WriteToChat("row: " + row + " column:" + col);
@@ -52,58 +57,70 @@ namespace UtilityBelt.Tools {
             try {
                 Match match = EmuConfigRegex.Match(e.Text);
                 Match configChange = EmuConfigChangedRegex.Match(e.Text);
-
-                if (match.Success) {
-                    //e.Eat = true;
-
-                    string configHeader = match.Groups["ConfigHeader"].Value;
-                    string configValues = match.Groups["ConfigValues"].Value;
-
-                    string[] configValuesArray =  (configValues.Split(','));
-
-                    HudList.HudListRowAccessor newHeaderRow = UIEmuConfigList.AddRow();
-                    ((HudStaticText)newHeaderRow[0]).Text = configHeader.Trim() + " settings";
-                    ((HudStaticText)newHeaderRow[0]).TextColor = System.Drawing.Color.Goldenrod;
-                    ((HudCheckBox)newHeaderRow[1]).Visible = false;
+            //ChatHandler:
+                if (DateTime.UtcNow - lastRequestedConfigList < TimeSpan.FromSeconds(2)) {
+                    if (match.Success) {
+                        e.Eat = true;
 
 
-                    Util.WriteToChat(configHeader.Trim());
-                    foreach(string configValue in configValuesArray) {
+                        string configHeader = match.Groups["ConfigHeader"].Value;
+                        string configValues = match.Groups["ConfigValues"].Value;
 
-                        if (Enum.IsDefined(typeof(CharOptions),configValue.Trim())) {
-                            int value = (int)Enum.Parse(typeof(CharOptions), configValue.Trim());
-                            //if ((Globals.Core.CharacterFilter.CharacterOptions & 0x00000002) != 0) { "this setting is on" };
-                            if ((Globals.Core.CharacterFilter.CharacterOptions & value) != 0) {
-                                HudList.HudListRowAccessor newValueRow = UIEmuConfigList.AddRow();
-                                ((HudStaticText)newValueRow[0]).Text = "        " + Regex.Replace(configValue, "([A-Z]+)", " $1").Trim();
-                                ((HudCheckBox)newValueRow[1]).Checked = true;
-                                Util.WriteToChat("       " + configValue.Trim());
-                                Util.WriteToChat(configValue.Trim() + " status: On");
-                            } else if (Enum.IsDefined(typeof(CharOptions2), configValue.Trim())) {
-                                HudList.HudListRowAccessor newValueRow = UIEmuConfigList.AddRow();
-                                ((HudStaticText)newValueRow[0]).Text = "        " + Regex.Replace(configValue, "([A-Z]+)", " $1").Trim();
-                                ((HudCheckBox)newValueRow[1]).Checked = true;
-                                Util.WriteToChat("       " + configValue.Trim());
-                                Util.WriteToChat(configValue.Trim() + " status: On");
+                        string[] configValuesArray = (configValues.Split(','));
+
+                        HudList.HudListRowAccessor newHeaderRow = UIEmuConfigList.AddRow();
+                        ((HudStaticText)newHeaderRow[0]).Text = configHeader.Trim() + " settings";
+                        ((HudStaticText)newHeaderRow[0]).TextColor = System.Drawing.Color.Goldenrod;
+                        ((HudCheckBox)newHeaderRow[1]).Visible = false;
+
+
+                        //Util.WriteToChat(configHeader.Trim());
+                        foreach (string configValue in configValuesArray) {
+                            //Util.WriteToChat(configValue.Trim());
+
+
+                            if (Enum.IsDefined(typeof(CharOptions), configValue.Trim())) {
+                                uint charOptionsValue = (uint)Enum.Parse(typeof(CharOptions), configValue.Trim());
+                                //if ((Globals.Core.CharacterFilter.CharacterOptions & 0x00000002) != 0) { "this setting is on" };
+                                if ((uint)(Globals.Core.CharacterFilter.CharacterOptions & charOptionsValue) != 0) {
+                                    HudList.HudListRowAccessor newValueRow = UIEmuConfigList.AddRow();
+                                    ((HudStaticText)newValueRow[0]).Text = "        " + Regex.Replace(configValue, "([A-Z]+)", " $1").Trim();
+                                    ((HudCheckBox)newValueRow[1]).Checked = true;
+                                    //Util.WriteToChat("       " + configValue.Trim());
+                                    //Util.WriteToChat(configValue.Trim() + " status: On");
+                                } else {
+                                    HudList.HudListRowAccessor newValueRow = UIEmuConfigList.AddRow();
+                                    ((HudStaticText)newValueRow[0]).Text = "        " + Regex.Replace(configValue, "([A-Z]+)", " $1").Trim();
+                                    ((HudCheckBox)newValueRow[1]).Checked = false;
+                                    //Util.WriteToChat(configValue.Trim() + " status: Off");
+                                }
                             }
-                            else {
-                                HudList.HudListRowAccessor newValueRow = UIEmuConfigList.AddRow();
-                                ((HudStaticText)newValueRow[0]).Text = "        " + Regex.Replace(configValue, "([A-Z]+)", " $1").Trim();
-                                ((HudCheckBox)newValueRow[1]).Checked = false;
-                                Util.WriteToChat(configValue.Trim() + " status: Off");
+                            if (Enum.IsDefined(typeof(CharOptions2), configValue.Trim())) {
+                                uint charOptions2Value = (uint)Enum.Parse(typeof(CharOptions2), configValue.Trim());
+                                if ((Globals.Core.CharacterFilter.CharacterOptionFlags & charOptions2Value) != 0) {
+                                    HudList.HudListRowAccessor newValueRow = UIEmuConfigList.AddRow();
+                                    ((HudStaticText)newValueRow[0]).Text = "        " + Regex.Replace(configValue, "([A-Z]+)", " $1").Trim();
+                                    ((HudCheckBox)newValueRow[1]).Checked = true;
+                                    //Util.WriteToChat("       " + configValue.Trim());
+                                    //Util.WriteToChat(configValue.Trim() + " status: On");
+                                } else {
+                                    HudList.HudListRowAccessor newValueRow = UIEmuConfigList.AddRow();
+                                    ((HudStaticText)newValueRow[0]).Text = "        " + Regex.Replace(configValue, "([A-Z]+)", " $1").Trim();
+                                    ((HudCheckBox)newValueRow[1]).Checked = false;
+                                    //Util.WriteToChat(configValue.Trim() + " status: Off");
+                                }
                             }
-                            
                         }
+                    } else {
+                        //Util.WriteToChat("did not find" + e.Text);
                     }
                 }
-                else {
-                    //Util.WriteToChat(e.Text);
-                }
+                
             if (configChange.Success) {
                 string changedConfig = configChange.Groups["changedConfig"].Value;
                 string changedStatus = configChange.Groups["changedStatus"].Value;
                 bool changedStatusBool;
-                    Util.WriteToChat("changedConfig: " + changedConfig + " changedStatus: " + changedStatus);
+                    //Util.WriteToChat("changedConfig: " + changedConfig + " changedStatus: " + changedStatus);
                     if (changedStatus == "on") {
                         changedStatusBool = true;
                     } else {
