bc64d6c92dc5fbded9671c38ac94e6feb1274937
trevis <trevis@haxit.org>
2019-09-21 10:39:33 -0600

    Assessor now skips items that dont need to be identified (I hope this is the case? heh) SpellComps/Money/ETc

--- Tools/Assessor.cs
+++ Tools/Assessor.cs
@@ -2,24 +2,43 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Decal.Adapter.Wrappers;
 
 namespace UtilityBelt.Tools {
     public static class Assessor {
         private static DateTime lastThought = DateTime.MinValue;
         public static int itemsNeedingData = 0;
 
+        private static List<ObjectClass> SkippableObjectClasses = new List<ObjectClass>() {
+            ObjectClass.Money,
+            ObjectClass.TradeNote,
+            ObjectClass.Salvage,
+            ObjectClass.Scroll,
+            ObjectClass.SpellComponent,
+            ObjectClass.Container,
+            ObjectClass.Foci,
+            ObjectClass.Food,
+            ObjectClass.Plant
+        };
+
         public static bool NeedsInventoryData() {
             bool needsData = false;
             itemsNeedingData = 0;
 
             foreach (var wo in Globals.Core.WorldFilter.GetInventory()) {
-                if (!wo.HasIdData) {
+                if (!wo.HasIdData && ItemNeedsIdData(wo)) {
                     needsData = true;
                     itemsNeedingData++;
                 }
             }
 
             return needsData;
+        }
+
+        private static bool ItemNeedsIdData(WorldObject wo) {
+            if (SkippableObjectClasses.Contains(wo.ObjectClass)) return false;
+
+            return true;
         }
 
         public static int GetNeededIdCount() {
@@ -38,7 +57,7 @@ namespace UtilityBelt.Tools {
             itemsNeedingData = 0;
 
             foreach (var wo in Globals.Core.WorldFilter.GetInventory()) {
-                if (!wo.HasIdData) {
+                if (!wo.HasIdData && ItemNeedsIdData(wo)) {
                     Globals.Core.Actions.RequestId(wo.Id);
 
                     itemsNeedingData++;
