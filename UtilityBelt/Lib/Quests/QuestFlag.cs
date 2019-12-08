74dfd857471bafd3275567bd68d5b52c98d5d129
Trevis <3235171-trevis@users.noreply.gitlab.com>
2019-12-08 22:57:22 +0000

    TESTING: allow deltas bigger than 6 digits long from myquests...

--- UtilityBelt/Lib/Quests/QuestFlag.cs
+++ UtilityBelt/Lib/Quests/QuestFlag.cs
@@ -8,7 +8,7 @@ using System.Xml;
 
 namespace UtilityBelt.Lib.Quests {
     class QuestFlag : IComparable {
-        public static readonly Regex MyQuestRegex = new Regex(@"(?<key>\S+) \- (?<solves>\d+) solves \((?<completedOn>\d{0,11})\)""?((?<description>.*)"" (?<maxSolves>.*) (?<repeatTime>\d{0,6}))?.*$");
+        public static readonly Regex MyQuestRegex = new Regex(@"(?<key>\S+) \- (?<solves>\d+) solves \((?<completedOn>\d{0,11})\)""?((?<description>.*)"" (?<maxSolves>.*) (?<repeatTime>\d{0,11}))?.*$");
         public static readonly Regex KillTaskRegex = new Regex(@"killtask|killcount|slayerquest|totalgolem.*dead");
 
         public static Dictionary<string, string> FriendlyNamesLookup = new Dictionary<string, string>();
