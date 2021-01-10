425096af89a6e12c397e7b1e54dcd53cd6fd1a98
trevis <trevis@haxit.org>
2021-01-09 20:24:27 -0700

    add missing vtank meta view expressions: uigetcontrol, uisetlabel, uisetvisible

---
+++ UtilityBelt/Lib/Expressions/ExpressionUIControl.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace UtilityBelt.Lib.Expressions {
+    public class ExpressionUIControl : ExpressionObjectBase {
+        [JsonIgnore]
+        public string WindowName { get; set; }
+        [JsonIgnore]
+        public string ControlName { get; set; }
+
+        public ExpressionUIControl() {
+            IsSerializable = false;
+        }
+
+        public ExpressionUIControl(string windowName, string controlName) {
+            IsSerializable = false;
+            ShouldFireEvents = false;
+            WindowName = windowName;
+            ControlName = controlName;
+            ShouldFireEvents = true;
+        }
+
+        public override string ToString() {
+            return $"{WindowName}::{ControlName}";
+        }
+    }
+}
